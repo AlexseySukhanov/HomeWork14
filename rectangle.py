294e077dae5db40567c2f7c95f5a16a5d3452590
Alexsey <alexseysukhanov@gmail.com>
2021-11-08 17:20:23 +0200

    fix

--- rectangle.py
+++ rectangle.py
@@ -7,31 +7,43 @@ class Rectangle:
         return str(self.w * self.h)
 
     def __gt__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, Rectangle):
             raise TypeError(f'{type(other).__name__} Object is not valid')
         return self.w * self.h > other.w * other.h
 
     def __lt__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, Rectangle):
             raise TypeError(f'{type(other).__name__} Object is not valid')
         return self.w * self.h < other.w * other.h
 
     def __eq__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, Rectangle):
             raise TypeError(f'{type(other).__name__} Object is not valid')
         return self.w * self.h == other.w * other.h
 
     def __ne__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, Rectangle):
             raise TypeError(f'{type(other).__name__} Object is not valid')
         return self.w * self.h != other.w * other.h
 
     def __add__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, Rectangle):
             raise TypeError(f'{type(other).__name__} Object is not valid')
         return self.w * self.h + other.w * other.h
 
     def __mul__(self, other):
+        if not isinstance(self, Rectangle):
+            raise TypeError(f'{type(self).__name__} Object is not valid')
         if not isinstance(other, (int,float)):
             raise TypeError(f'{type(other).__name__} Multipyer is not valid')
         return self.w * self.h * other
